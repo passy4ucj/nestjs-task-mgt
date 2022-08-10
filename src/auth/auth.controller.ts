065bf3d0d13f9541b55b3202e200c8662b7f9ee8
passy4ucj <ojinnnakapascal@gmail.com>
2022-08-10 09:03:34 +0100

    Get user decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
 import { Body, Controller, Post, Req, UseGuards, ValidationPipe } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+// import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+// import { GetUser } from './get-user.decorator';
+// import { User } from './user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -20,9 +22,9 @@ export class AuthController {
         return await this.authService.signIn(authCredentialsDto)
     }
 
-    @Post('/test')
-    @UseGuards(AuthGuard())
-    test(@Req() req) {
-        console.log(req)
-    }
+    // @Post('/test')
+    // @UseGuards(AuthGuard())
+    // test(@GetUser() user: User) {
+    //     console.log(user)
+    // }
 }
