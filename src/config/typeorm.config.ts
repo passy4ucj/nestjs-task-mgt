fc9717d1ebc691e6f1c193bf875fd354f6420119
passy4ucj <ojinnnakapascal@gmail.com>
2022-08-14 09:32:51 +0100

    Introduction to cinfiguration and applying configuration in project

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,14 +1,16 @@
-import { TypeOrmModuleOptions } from "@nestjs/typeorm";
-import { Task } from "src/tasks/task.entity";
+import { TypeOrmModuleOptions } from "@nestjs/typeorm"
+import * as config from 'config'
+
+const dbConfig = config.get('db')
 
 export const typeOrmConfig: TypeOrmModuleOptions  = {
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'taskmanagement',
-    synchronize: true,
+    type: dbConfig.type,
+    host: process.env.RDS_HOSTNAME || dbConfig.host,
+    port: process.env.RDS_PORT || dbConfig.port,
+    username: process.env.RDS_USERNAME || dbConfig.username,
+    password: process.env.RDS_PASSWORD || dbConfig.password,
+    database: process.env.RDS_DB_NAME || dbConfig.database,
+    synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize,
     // entities: [__dirname + '/entities/*.ts'],
     // entities: [__dirname + '/../**/*.entity{.ts,.js}'],
     // entities: [__dirname + '../**/*.entity{.ts,.js}'],
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common'
 import { AppModule } from './app.module';
+import * as config from 'config'
+
 
 async function bootstrap() {
-
+  const serverConfig = config.get('server')
   const logger = new Logger('bootstrap')
 
-  const port = 3000
+
+
+  // console.log('NEW', serverConfig)
+  // console.log('DB', config.get('db'))
+  const port = process.env.PORT || serverConfig.port
   const app = await NestFactory.create(AppModule);
+  
   await app.listen(3000);
   logger.log(`Application listening on port ${port}`)
 }
